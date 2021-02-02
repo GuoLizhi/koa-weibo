de0538523cf410ebda1f41da0015722e97a553bf
GuoLizhi <lizhi.guo@foxmail.com>
2021-02-02 21:41:18 +0800

    feat: add jest

--- src/db/seq.js
+++ src/db/seq.js
@@ -1,11 +1,15 @@
 const Sequelize = require('sequelize')
 const { MYSQL_CONF } = require('../conf/db')
-const { isProd } = require('../utils/env')
+const { isProd, isTest } = require('../utils/env')
 
 const { database, user, password, host } = MYSQL_CONF
 const conf = {
   host,
   dialect: 'mysql'
+}
+
+if (isTest) {
+  conf.logging = () => {}
 }
 
 if (isProd) {
